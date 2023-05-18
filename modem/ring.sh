2b22387d5fe58b16fe34ef5bff9595937fe33bc1
Todd Short <todd.short@me.com>
2023-05-18 19:42:46 -0400

    Move stuff into subdirectories

--- modem/ring.sh
+++ modem/ring.sh
@@ -3,5 +3,5 @@
 # echo -e "ATS7=30A\\r" >> /dev/cu.usbmodem123456781
 # echo -e "ATS6=30D\\r" >> /dev/cu.usbmodem123456781
 # echo -e "ATZ0H1\\r" >> /dev/cu.usbmodem123456781
-/Users/tshort/git/qlab/modem /dev/cu.usbmodem123456781 "ATH1" 20
-/Users/tshort/git/qlab/modem /dev/cu.usbmodem123456781 "ATH0Z0"
+~/git/qlab/modem/modem /dev/cu.usbmodem123456781 "ATH1" 20
+~/git/qlab/modem/modem /dev/cu.usbmodem123456781 "ATH0Z0"
