17a015f0c8b1887a4a358310a27dd7661f3082b6
Todd Short <todd.short@me.com>
2023-05-18 20:03:46 -0400

    Remove client variable

--- streamdeck/next.py
+++ streamdeck/next.py
@@ -1,4 +1,4 @@
 #!/usr/local/bin/python3
 from pythonosc.udp_client import SimpleUDPClient
 
-client = SimpleUDPClient("localhost", 53535).send_message("/NEXT", [])
+SimpleUDPClient("localhost", 53535).send_message("/NEXT", [])
--- streamdeck/pause.py
+++ streamdeck/pause.py
@@ -1,4 +1,4 @@
 #!/usr/local/bin/python3
 from pythonosc.udp_client import SimpleUDPClient
 
-client = SimpleUDPClient("localhost", 53535).send_message("/PAUSE", [])
+SimpleUDPClient("localhost", 53535).send_message("/PAUSE", [])
